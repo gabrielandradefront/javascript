0a228957b45f0f4511fc1948633442e6f26cf97c
Gabriel A. <gabriiellandrad@gmail.com>
2025-01-25 23:01:27 -0300

    nova fase do js

--- jsballerini/aula 2/script.js
+++ jsballerini/aula 2/script.js
@@ -1,13 +1,19 @@
 //function está criando a ação que vai ser executada
+
 function adicionarTarefa() {
+  let suaTarefa = document.getElementById("suaTarefa")
+
+  //Variaável que pega o valor dentro do elemento acima
+  let tarefa = suaTarefa.value.trim()
+
+  if (tarefa == "") {
+    window.alert("Escreva uma atividade no campo abaixo")
+
+  } else {
 
     let mensagem = "Tarefa Adicionada"
 
     //Variável do elemento input (lugar onde tem entrada de informação)
-    let suaTarefa = document.getElementById("suaTarefa")
-
-    //Variaável que pega o valor dentro do elemento acima
-    let tarefa = suaTarefa.value
 
     document.getElementById("mensagem").textContent = mensagem;
 
@@ -21,6 +27,7 @@ function adicionarTarefa() {
     //posiciona o elemento li no lugar certo, sem o posicionamento a tag não funciona
     listaT.appendChild(novaT)
 
-    suaTarefa.value = ""
+    suaTarefa.value = "" 
+  }
   }
   
